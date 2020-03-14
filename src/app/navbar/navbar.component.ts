3646e43cf431a259358dea14caa39bc7cfd56cd1
skocic <skocic@whow.net>
2020-03-14 11:59:17 +0000

    Revert "promena"
    This reverts commit 925cd5f7d1f4d44c6d3a72d6835a53a97a879457

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent  {
+  
+  constructor(public authService: AuthService ) { 
+        
+  }
+
+  logout(){
+    // this.authService.afAuth.auth.signOut();
+          this.authService.logout();  
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyChCdUJuR0McvQ0F_OWEs6yuS1Zja2sXWg",
+    authDomain: "alpha-b1bb8.firebaseapp.com",
+    databaseURL: "https://alpha-b1bb8.firebaseio.com",
+    projectId: "alpha-b1bb8",
+    storageBucket: "alpha-b1bb8.appspot.com",
+    messagingSenderId: "319189789495",
+    appId: "1:319189789495:web:3bdd0cd62bd7003e5548b2",
+    measurementId: "G-4V9WNDLBN2"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
